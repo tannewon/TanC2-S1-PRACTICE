a62eba908ed6c91ad210f578473eb02530a1462f
Tan <tan.a25@student.passerellesnumeriques.org>
2024-06-14 16:41:54 +0700

    Step 2 second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { HTML_RESULTS, CSS_RESULTS } from './data'; // Import data
 const App = () => {
   return (
     <div className="App">
-      <Header batchName="Spring 2024" />
+      <Header batchName="Student result for PNC batch 2024" />
       <main>
         <Scores courseName="HTML" courseResults={HTML_RESULTS} />
         <Scores courseName="CSS" courseResults={CSS_RESULTS} />
